cbd8f56f49e2b2152b14a528d6c3ea1ba4433f9e
KW <kenneth_sl_wong@msn.com>
2022-03-08 16:10:43 -0500

    added connection to database

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '?msqlKW?',
+    database: 'election'
+},
+console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
